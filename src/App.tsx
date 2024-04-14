58fe3d751cbea0093cbbe9ef20fa38e512dd6004
Argiro <elto.82@gmai.com>
2024-04-13 20:51:47 -0500

    sin react query o tanstack

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { useEffect, useReducer, useState } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const getRandomNumberFromApi = async (): Promise<number> => {
+  const res = await fetch("https://www.random.org/integers/?num=1&min=1&max=500&col=1&base=10&format=plain&rnd=new")
+  const numberString = await res.text()
+  //throw new Error('Help me')
+
+  return +numberString
+
+}
+
+const App = () => {
+
+  const [number, setNumber] = useState<number>()
+  const [isLoading, setIsLoading] = useState<boolean>(true)
+  const [error, setError] = useState<string>()
+  const [key, forceRefresh] = useReducer((x) => x + 1, 0)
+
+  useEffect(() => {
+    setIsLoading(true)
+    getRandomNumberFromApi()
+      .then(num => setNumber(num))
+      .catch(error => setError(error.message))
+  }, [key])
+
+  useEffect(() => {
+    if (number) setIsLoading(false)
+  }, [number])
+
+  useEffect(() => {
+    if (error) setIsLoading(false)
+  }, [error])
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <div className="card read-the-docs">
+        {
+          isLoading ? (<h2>Loading...</h2>)
+            : !error && <h2>Number Random:{number} </h2>
+        }
+        {
+          !isLoading && error && <h3>{error} </h3>
+        }
+
+        <button onClick={forceRefresh} disabled={isLoading}>
+          {
+            isLoading ? 'Loading...' : 'Generate new number'
+          }
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </>
   )
 }
